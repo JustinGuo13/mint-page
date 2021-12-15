e0d496193bbc1d4a9f7babf856b9b92a038f48cb
Peng Yang Guo <pyangguo@gmail.com>
2021-12-15 12:50:59 -0800

    Create NFT collection

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-// Work on NFT collection ...
+// Work on NFT collection
 export default function Home() {
 	return (
 		<div className={styles.container}>
